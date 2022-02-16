2bc2585b7c0dc35b627676be2ec54707c6316a33
Jost von Hardenberg <hardenberg@gmail.com>
2022-02-16 22:41:01 +0000

    cleanup using pathlib

--- python_global_mean.py
+++ python_global_mean.py
@@ -11,6 +11,7 @@ import netCDF4 as nc
 import argparse
 from tabulate import tabulate
 from cdo import *
+from pathlib import Path
 cdo = Cdo()
 
 # arguments
@@ -27,34 +28,34 @@ year2 = args.year2
 with open("config.yml", "r") as file:
     cfg = yaml.load(file, Loader=yaml.FullLoader)
 
-ECEDIR = os.path.join(cfg["dirs"]["exp"], expname)
-TABDIR = os.path.join(cfg["dirs"]["tab"], "ECmean4", "table")
-TMPDIR = os.path.join(cfg["dirs"]["tmp"], "ECmean4", "tmp")
+ECEDIR = Path(cfg["dirs"]["exp"], expname)
+TABDIR = Path(cfg["dirs"]["tab"], "ECmean4", "table")
+TMPDIR = Path(cfg["dirs"]["tmp"], "ECmean4", "tmp")
+#ECEDIR = os.path.join(cfg["dirs"]["exp"], expname)
+#TABDIR = os.path.join(cfg["dirs"]["tab"], "ECmean4", "table")
+#TMPDIR = os.path.join(cfg["dirs"]["tmp"], "ECmean4", "tmp")
 os.makedirs(TABDIR, exist_ok=True)
 os.makedirs(TMPDIR, exist_ok=True)
-eceinitfile = os.path.join(ECEDIR, "ICMGG" + expname + "INIT")
+eceinitfile = ECEDIR / f"ICMGG{expname}INIT"
+
 #cdo.forceOutput = True
 #cdo.debug = True
 vardict = {}
 
 # loop
-var_field = cfg["vars"]["field"]
-var_radiation = cfg["vars"]["radiation"]
+var_field = cfg["atm_vars"]["field"]
+var_radiation = cfg["atm_vars"]["radiation"]
 for var in var_field + var_radiation:
     for year in range(year1, year2+1):
         print(var, year)
-        infile = os.path.join(ECEDIR, "output/oifs", expname +
-                              "_atm_cmip6_1m_" + str(year) + "-" + str(year) + ".nc")
-        outfile = os.path.join(TMPDIR, "tmp_" + str(year) + ".nc")
+        infile = ECEDIR / "output/oifs" / f"{expname}_atm_cmip6_1m_{year}-{year}.nc"
+        outfile = TMPDIR / f"tmp_{year}.nc"
         # regrid and select variable
-        cdo.fldmean(input="-setgridtype,regular -setgrid," +
-                    eceinitfile + " -selname," + var + " " + infile, output=outfile)
+        cdo.fldmean(input=f"-setgridtype,regular -setgrid,{eceinitfile} -selname,{var} {infile}", output=str(outfile))
     # cat and timmean
-    cdo.timmean(input="-cat " + os.path.join(TMPDIR, "tmp*.nc"),
-                output=os.path.join(TMPDIR, var + "_mean.nc"))
+    cdo.timmean(input=f"-cat {TMPDIR}/tmp*.nc", output=f"{TMPDIR}/{var}_mean.nc")
     # store mean value in a local dictionary
-    vardict[var] = float(nc.Dataset(os.path.join(
-        TMPDIR, var + "_mean.nc")).variables[var][:])
+    vardict[var] = float(nc.Dataset( TMPDIR / f"{var}_mean.nc").variables[var][:])
 
 # extra radiative variables
 extra_radiation = ["net_toa", "net_sfc"]
@@ -82,11 +83,10 @@ for var in var_field + var_radiation + extra_radiation:
     global_table.append(out_sequence)
 
 # write the file  with tabulate: cool python feature
-tablefile = os.path.join(TABDIR, "Global_Mean_" +
-                         expname + "_" + str(year1) + "_" + str(year2) + ".txt")
+tablefile = TABDIR / f"Global_Mean_{expname}_{year1}_{year2}.txt"
 with open(tablefile, 'w') as f:
     f.write(tabulate(global_table, headers=head, tablefmt="orgtbl"))
 
 # clean
-for f in glob.glob(os.path.join(TMPDIR, "*.nc")):
+for f in TMPDIR.glob("*.nc"):
     os.remove(f)
