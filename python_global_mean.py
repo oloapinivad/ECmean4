f27e03832b9c499b2df4c70c59c0848bb816a768
Paolo Davini <ccpd@aa6-100.bullx>
2022-02-17 12:19:27 +0000

    further updates, toa-sfc

--- python_global_mean.py
+++ python_global_mean.py
@@ -7,7 +7,6 @@ import sys
 import os
 import yaml
 import numpy as np
-#import netCDF4 as nc
 import argparse
 from tabulate import tabulate
 from statistics import mean
@@ -16,7 +15,8 @@ from pathlib import Path
 cdo = Cdo()
 
 # arguments
-parser = argparse.ArgumentParser(description='ECmean global mean diagnostics for EC-Earth4')
+parser = argparse.ArgumentParser(
+    description='ECmean global mean diagnostics for EC-Earth4')
 parser.add_argument('exp', metavar='EXP', type=str, help='experiment ID')
 parser.add_argument('year1', metavar='Y1', type=int, help='starting year')
 parser.add_argument('year2', metavar='Y2', type=int, help='final year')
@@ -32,15 +32,13 @@ with open(indir / "config.yml", "r") as file:
 
 ECEDIR = Path(cfg["dirs"]["exp"], expname)
 TABDIR = Path(cfg["dirs"]["tab"], "ECmean4", "table")
-#TMPDIR = Path(cfg["dirs"]["tmp"], "ECmean4", "tmp")
 os.makedirs(TABDIR, exist_ok=True)
-#os.makedirs(TMPDIR, exist_ok=True)
 
 #cdo.forceOutput = True
 #cdo.debug = True
 
 # prepare grid description file
-gridfile=str(TABDIR / "grid.txt")
+gridfile = str(TABDIR / "grid.txt")
 griddes = cdo.griddes(input=str(ECEDIR / f"ICMGG{expname}INIT"))
 with open(gridfile, "w") as f:
     for line in griddes:
@@ -59,25 +57,22 @@ var_radiation = cfg["atm_vars"]["radiation"]
 for var in var_field + var_radiation:
     a = []
     if 'derived' in ref[var].keys():
-       cmd = ref[var]['derived']
-       der = f" -expr,{var}={cmd} "
-    else: 
-       der = ""
+        cmd = ref[var]['derived']
+        der = f" -expr,{var}={cmd} "
+    else:
+        der = ""
     for year in range(year1, year2+1):
-        infile = ECEDIR / "output/oifs" / f"{expname}_atm_cmip6_1m_{year}-{year}.nc"
-#        cmd = f"-timmean -setgridtype,regular -setgrid,{gridfile} -selname,{var} {infile}"
-        cmd = f"-timmean -zonmean -setgrid,{gridfile} -selname,{var} {der} {infile}" # Equivalent, faster
-        x=cdo.fldmean(input=cmd, returnCdf = True).variables[var][:]
-        a.append(x.item())
+        infile = ECEDIR / "output/oifs" / \
+            f"{expname}_atm_cmip6_1m_{year}-{year}.nc"
+        #cmd = f"-timmean -zonmean -setgrid,{gridfile} -selname,{var} {der} {infile}"
+        #x=cdo.fldmean(input=cmd, returnCdf = True).variables[var][:]
+        # a.append(x.item())
+        # simpler with cdo output
+        cmd = f"-timmean -fldmean -setgrid,{gridfile} -selname,{var} {der} {infile}"
+        x = float(cdo.output(input=cmd)[0])
+        a.append(x)
     varstat[var] = mean(a)
     print("Average", var, mean(a))
-
-# extra radiative variables
-#! Not needed anymore: now computed as derived variables as specified in reference.yml
-#extra_radiation = ["net_toa", "net_sfc"]
-#varstat["net_toa"] = varstat["rsnt"] + varstat["rlnt"]
-#varstat["net_sfc"] = varstat["rsns"] + varstat["rlns"] - varstat["hfls"] - varstat["hfss"]
-
 
 # define options for the output table
 head = ["Var", "Longname", "Units", "ECE4", "OBS", "Obs Dataset"]
@@ -97,8 +92,3 @@ tablefile = TABDIR / f"Global_Mean_{expname}_{year1}_{year2}.txt"
 print(tablefile)
 with open(tablefile, 'w') as f:
     f.write(tabulate(global_table, headers=head, tablefmt="orgtbl"))
-
-# clean
-#os.unlink(gridfile)
-#for f in TMPDIR.glob("*.nc"):
-#    os.remove(f)
