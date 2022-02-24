b6a83e1bb405c53f0bc3ba2e3e854efe09236fa9
Jost von Hardenberg <hardenberg@gmail.com>
2022-02-24 23:28:12 +0000

    linting

--- global_mean.py
+++ global_mean.py
@@ -3,16 +3,14 @@
 # python3 version of ECmean global mean tool
 # It uses a reference file from yaml and cdo bindings
 
-import sys
 import os
-import yaml
-import numpy as np
-import argparse
 import re
-from tabulate import tabulate
+import argparse
 from statistics import mean
-from cdo import *
 from pathlib import Path
+import yaml
+from tabulate import tabulate
+from cdo import Cdo
 
 def is_number(s):
     try:
@@ -27,9 +25,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument('exp', metavar='EXP', type=str, help='experiment ID')
 parser.add_argument('year1', metavar='Y1', type=int, help='starting year')
 parser.add_argument('year2', metavar='Y2', type=int, help='final year')
-parser.add_argument('-s', '--silent', action='store_true', help='do not print anything to std output')
-parser.add_argument('-t', '--table', action='store_true', help='appends also single line to a table')
-parser.add_argument('-o', '--output', type=str, default='',  help='path of output one-line table')
+parser.add_argument('-s', '--silent', action='store_true',
+                    help='do not print anything to std output')
+parser.add_argument('-t', '--table', action='store_true',
+                    help='appends also single line to a table')
+parser.add_argument('-o', '--output', type=str, default='',
+                    help='path of output one-line table')
 args = parser.parse_args()
 expname = args.exp
 year1 = args.year1
@@ -50,20 +51,20 @@ TMPDIR = Path(cfg['dirs']['tmp'])
 os.makedirs(TABDIR, exist_ok=True)
 
 # prepare grid description file
-gridfile=str(TMPDIR / 'grid.txt')
-inifile=str(ECEDIR / f'ICMGG{expname}INIT')
-griddes = cdo.griddes(input=inifile)
-with open(gridfile, 'w') as f:
+GRIDFILE=str(TMPDIR / 'grid.txt')
+INIFILE=str(ECEDIR / f'ICMGG{expname}INIT')
+griddes = cdo.griddes(input=INIFILE)
+with open(GRIDFILE, 'w') as f:
     for line in griddes:
         print(line, file=f)
 
 # prepare LSM
-lmfile=str(TMPDIR / 'lmask.nc')
-smfile=str(TMPDIR / 'smask.nc')
-gafile=str(TMPDIR / 'ga.nc')
-cdo.selname('LSM', input=f'-setgridtype,regular {inifile}', output=lmfile, options='-t ecmwf')
-cdo.mulc('-1', input=f'-subc,1 {lmfile}', output=smfile)  
-cdo.gridarea(input=f'-setgridtype,regular {lmfile}', output=gafile)
+LMFILE=str(TMPDIR / 'lmask.nc')
+SMFILE=str(TMPDIR / 'smask.nc')
+GAFILE=str(TMPDIR / 'ga.nc')
+cdo.selname('LSM', input=f'-setgridtype,regular {INIFILE}', output=LMFILE, options='-t ecmwf')
+cdo.mulc('-1', input=f'-subc,1 {LMFILE}', output=SMFILE)
+cdo.gridarea(input=f'-setgridtype,regular {LMFILE}', output=GAFILE)
 
 # reference data
 with open(INDIR / 'reference.yml', 'r') as file:
@@ -80,25 +81,25 @@ var_table = cfg['global']['tab_vars']
 # create a list for all variable, avoid duplicates
 var_all = list(set(var_field + var_radiation + var_table))
 
-infile = str(ECEDIR / 'output/oifs' / f'{expname}_atm_cmip6_1m_{year1}-{year1}.nc')
-var_avail = [v.split()[1] for v in cdo.pardes(input=infile)]
+INFILE = str(ECEDIR / 'output/oifs' / f'{expname}_atm_cmip6_1m_{year1}-{year1}.nc')
+var_avail = [v.split()[1] for v in cdo.pardes(input=INFILE)]
 
 isavail = {}
 for v in var_all:
-     isavail[v] = True
-     d = ref[v].get('derived')
-     if d:
-         var_req = re.split('[\*+-]', d)
-         for x in var_req:
-             if is_number(x):
-                 var_req.remove(x)
-     else:
-         var_req = [v] 
+    isavail[v] = True
+    d = ref[v].get('derived')
+    if d:
+        var_req = re.split('[*+-]', d)
+        for x in var_req:
+            if is_number(x):
+                var_req.remove(x)
+    else:
+        var_req = [v]
 
-     for x in var_req:
-         if x not in var_avail:
-             isavail[v] = False
-             print(f"Variable {x} needed by {v} is not available in the model output!")
+    for x in var_req:
+        if x not in var_avail:
+            isavail[v] = False
+            print(f"Variable {x} needed by {v} is not available in the model output!")
 
 # loop
 varstat = {}
@@ -114,32 +115,34 @@ for var in var_all:
             der = f'-expr,{var}={cmd}'
         else:
             der = ''
-    
+
         # land/sea variables
         mask = ''
         op='-fldmean'
         if 'total' in ref[var].keys():
             mask_type = ref[var]['total']
             if mask_type == 'land':
-                mask = f'-mul {gafile} -mul {lmfile}'
+                mask = f'-mul {GAFILE} -mul {LMFILE}'
                 op='-fldsum'
             elif mask_type in ['sea', 'ocean']:
-                mask = f'-mul {gafile} -mul {smfile}'
+                mask = f'-mul {GAFILE} -mul {SMFILE}'
                 op='-fldsum'
 
         # loop on years: call CDO to perform all the computations
         for year in range(year1, year2+1):
-            infile = ECEDIR / 'output/oifs' / \
+            INFILE = ECEDIR / 'output/oifs' / \
                f'{expname}_atm_cmip6_1m_{year}-{year}.nc'
-            cmd = f'-timmean {op} {mask} -setgridtype,regular -setgrid,{gridfile} -selname,{var} {der} {infile}'
+            cmd = f'-timmean {op} {mask} -setgridtype,regular ' \
+                  f'-setgrid,{GRIDFILE} -selname,{var} {der} {INFILE}'
             x = float(cdo.output(input=cmd)[0])
             a.append(x)
         varstat[var] = mean(a)
-        if fverb: print('Average', var, mean(a))
+        if fverb:
+            print('Average', var, mean(a))
 
 # define options for the output table
 head = ['Variable', 'Longname', 'Units', 'EC-Earth4', 'Obs.', 'Dataset', 'Years']
-global_table = list()
+global_table = []
 
 # loop on the variables to create the table
 for var in var_field + var_radiation:
@@ -147,7 +150,7 @@ for var in var_field + var_radiation:
     beta['value'] = varstat[var] * float(beta['factor'])
     out_sequence = [var, beta['varname'], beta['units'], beta['value'],
                     float(beta['observations']['val']),
-                    beta['observations'].get('data',''), 
+                    beta['observations'].get('data',''),
                     beta['observations'].get('years','')]
     global_table.append(out_sequence)
 
@@ -160,7 +163,7 @@ with open(tablefile, 'w') as f:
 # Print appending one line to table (for tuning)
 if ftable:
     linefile = TABDIR / 'global_means.txt'
-    if args.output: 
+    if args.output:
         linefile = args.output
     if not os.path.isfile(linefile):
         with open(linefile, 'w') as f:
@@ -174,12 +177,12 @@ if ftable:
 
     with open(linefile, 'a') as f:
         print(expname,'{:4d} {:4d} '.format(year1, year2), end='', file=f)
-        for var in var_table: 
+        for var in var_table:
             print('{:12.5f}'.format(varstat[var] * ref[var]['factor']), end=' ', file=f)
         print(file=f)
 
 # clean
-os.unlink(gridfile)
-os.unlink(lmfile)
-os.unlink(smfile)
-os.unlink(gafile)
+os.unlink(GRIDFILE)
+os.unlink(LMFILE)
+os.unlink(SMFILE)
+os.unlink(GAFILE)
