501e5bc027791fe637b6a894fc1b6a28800d429d
Jost von Hardenberg <hardenberg@gmail.com>
2022-03-17 00:03:16 +0000

    fixes binary LSM. Directly to main since I forgot it in branch

--- cdopipe.py
+++ cdopipe.py
@@ -41,11 +41,12 @@ class CdoPipe:
                 print(line, file=f)
 
         # prepare ATM LSM
+        fix = '-setgridtype,regular -setgrid,{self.GRIDFILE}'
         self.LMFILE = self.cdo.selname('LSM',
-                                       input=f'-setgridtype,regular {atminifile}',
+                                       input=f'-gec,0.5 {fix} {atminifile}',
                                        options='-t ecmwf')
         self.SMFILE = self.cdo.mulc('-1', input=f'-subc,1 {self.LMFILE}')
-        self.GAFILE = self.cdo.gridarea(input=f'-setgridtype,regular {self.LMFILE}')
+        self.GAFILE = self.cdo.gridarea(input=f'{self.LMFILE}')
 
         # prepare OCE areas
         self.OCEGAFILE = self.cdo.expr('area=e1t*e2t', input=oceinifile)
