1e3e344dd6b966b8d1a9993fd43598ed68d637cf
oloapinivad <p.davini@isac.cnr.it>
2022-05-04 15:43:54 +0000

    reorganise tests, introduce amip testing

--- test/test_global_mean.py
+++ test/test_global_mean.py
@@ -1,6 +1,10 @@
 from global_mean import main
 from filecmp import cmp
 
-def test_global_mean():
-    main(['mock', '1990', '1990', '-j', '2', '-c', 'test/config.yml'])
-    assert cmp('test/table/global_mean_mock_1990_1990.txt', 'test/table/global_mean_mock_1990_1990.ref')
+def test_global_mean_coupled():
+    main(['cpld', '1990', '1990', '-j', '2', '-c', 'test/config.yml'])
+    assert cmp('test/table/global_mean_cpld_EC-Earth4_r1i1p1f1_1990_1990.txt', 'test/table/global_mean_cpld_1990_1990.ref')
+
+def test_global_mean_amip():
+    main(['amip', '1990', '1990', '-j', '2', '-c', 'test/config.yml'])
+    assert cmp('test/table/global_mean_amip_EC-Earth4_r1i1p1f1_1990_1990.txt', 'test/table/global_mean_amip_1990_1990.ref')
