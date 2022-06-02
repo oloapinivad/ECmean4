f21b871e2c0a0ffeef64a5149861ee3e6da75085
Jost von Hardenberg <jost.hardenberg@polito.it>
2022-06-02 11:47:01 +0200

    sftlf compare with 50

--- cdopipe.py
+++ cdopipe.py
@@ -101,7 +101,7 @@ class CdoPipe:
             self.SEAMASK = self.cdo.mulc('-1', input=f'-subc,1 {self.LANDMASK}')
         elif component == 'cmoratm':
             self.LANDMASK = self.cdo.selname('sftlf',
-                                             input=f'-gec,0.5 {extra} {self.atmfix} {atminifile}')
+                                             input=f'-gec,50 {extra} {self.atmfix} {atminifile}')
             self.SEAMASK = self.cdo.mulc('-1', input=f'-subc,1 {self.LANDMASK}')
 
     def make_atm_remap_weights(self, atminifile, remap_method, target):
