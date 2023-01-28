2e6406fbc656befde7e30c10e80abfddb04a6cb9
oloapinivad <p.davini@isac.cnr.it>
2023-01-28 19:23:45 +0100

    fix test

--- tests/test_performance_indices.py
+++ tests/test_performance_indices.py
@@ -27,8 +27,7 @@ def test_performance_indices_amip(clim):
 
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
 def test_cmd_performance_indices_CMIP6(clim):
-    subprocess.run(['performance_indices', 'historical', '1990', '1990', '-j', '2', '-c', 'tests/config_CMIP6.yml',
-                    '-k', clim, '-m', 'EC-Earth4', '-v', 'debug'])
+    subprocess.run(['performance_indices', 'historical', '1990', '1990', '-j', '2', '-c', 'tests/config_CMIP6.yml', '-k', clim, '-m', 'EC-Earth3', '-v', 'debug'])
     assert cmp('tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml',
                'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref')
 
