d4e1195a86e074471e8bfce37724bd4eefea9cf7
oloapinivad <p.davini@isac.cnr.it>
2023-09-29 15:30:54 +0200

    performance tests to element-wise comparison

--- tests/test_performance_indices.py
+++ tests/test_performance_indices.py
@@ -4,12 +4,15 @@
 # all run for both EC23 and RK08 climatologies
 import os
 import subprocess
-from filecmp import cmp
 import pytest
 import xarray as xr
+import yaml
 from ecmean.performance_indices import performance_indices
 from ecmean.libs.ncfixers import xr_preproc
-#from ecmean.libs.general import set_multiprocessing_start_method
+from ecmean.libs.general import are_dicts_equal
+
+# set tolerance
+tolerance = 1e-4
 
 # set up coverage env var
 env = {**os.environ, "COVERAGE_PROCESS_START": ".coveragerc"}
@@ -19,16 +22,26 @@ env = {**os.environ, "COVERAGE_PROCESS_START": ".coveragerc"}
 def test_performance_indices_cpld(clim):
 
     performance_indices('cpld', 1990, 1990, numproc=4, climatology=clim, config='tests/config.yml')
-    assert cmp('tests/table/PI4_' + clim + '_cpld_EC-Earth4_r1i1p1f1_1990_1990.yml',
-               'tests/table/PI4_' + clim + '_cpld_1990_1990.ref')
+    file1 = 'tests/table/PI4_' + clim + '_cpld_EC-Earth4_r1i1p1f1_1990_1990.yml'
+    file2 = 'tests/table/PI4_' + clim + '_cpld_1990_1990.ref'
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        data1 = yaml.safe_load(f1)
+        data2 = yaml.safe_load(f2)
+
+    assert are_dicts_equal(data1, data2, tolerance), "YAML files are not identical."
+
 
 # test on amip
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
 def test_performance_indices_amip(clim):
     performance_indices('amip', 1990, 1990, numproc=1, climatology=clim, config='tests/config.yml', outputdir='pluto')
+    file1 = 'pluto/YAML/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml'
+    file2 = 'tests/table/PI4_' + clim + '_amip_1990_1990.ref'
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        data1 = yaml.safe_load(f1)
+        data2 = yaml.safe_load(f2)
 
-    assert cmp('pluto/YAML/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml',
-               'tests/table/PI4_' + clim + '_amip_1990_1990.ref')
+    assert are_dicts_equal(data1, data2, tolerance), "YAML files are not identical."
 
 # test performance_indices from commnand line + debug
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
@@ -36,24 +49,64 @@ def test_cmd_performance_indices_CMIP6(clim):
     subprocess.run(['performance_indices', 'historical', '1990', '1990', '-j', '2', '-c',
                    'tests/config_CMIP6.yml', '-k', clim, '-m', 'EC-Earth3', '-v', 'debug'],
                     env=env, check=True)
-    assert cmp('tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml',
-               'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref')
+    file1 = 'tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml'
+    file2 = 'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref'
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        data1 = yaml.safe_load(f1)
+        data2 = yaml.safe_load(f2)
+
+    assert are_dicts_equal(data1, data2, tolerance), "YAML files are not identical."
 
 # test on amip but with access from xarray dataset
-
-
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
 def test_performance_indices_amip_xdataset(clim):
-    thefile = 'tests/table/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml'
-    if os.path.isfile(thefile):
-        os.remove(thefile)
+    file1 = 'tests/table/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml'
+    file2 = 'tests/table/PI4_' + clim + '_amip_1990_1990.ref'
+    if os.path.isfile(file1):
+        os.remove(file1)
     xfield = xr.open_mfdataset('tests/data/amip/output/oifs/*.nc', preprocess=xr_preproc)
     performance_indices('amip', 1990, 1990, numproc=2, climatology=clim,
                         config='tests/config.yml', xdataset=xfield)
-    assert cmp(thefile, 'tests/table/PI4_' + clim + '_amip_1990_1990.ref')
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        data1 = yaml.safe_load(f1)
+        data2 = yaml.safe_load(f2)
 
+    assert are_dicts_equal(data1, data2, tolerance), "YAML files are not identical."
+
+
+# #test on coupled
 # @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
-# def test_performance_indices_CMIP6(clim):
-#    performance_indices('historical', 1990, 1990, numproc = 2, climatology = clim, config = 'tests/config_CMIP6.yml')
-#    assert cmp('tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml',
-#               'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref')
+# def test_performance_indices_cpld_old(clim):
+
+#     performance_indices('cpld', 1990, 1990, numproc=4, climatology=clim, config='tests/config.yml')
+#     assert cmp('tests/table/PI4_' + clim + '_cpld_EC-Earth4_r1i1p1f1_1990_1990.yml',
+#                'tests/table/PI4_' + clim + '_cpld_1990_1990.ref')
+
+# # test on amip
+# @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
+# def test_performance_indices_amip(clim):
+#     performance_indices('amip', 1990, 1990, numproc=1, climatology=clim, config='tests/config.yml', outputdir='pluto')
+
+#     assert cmp('pluto/YAML/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml',
+#                'tests/table/PI4_' + clim + '_amip_1990_1990.ref')
+
+# # test performance_indices from commnand line + debug
+# @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
+# def test_cmd_performance_indices_CMIP6(clim):
+#     subprocess.run(['performance_indices', 'historical', '1990', '1990', '-j', '2', '-c',
+#                    'tests/config_CMIP6.yml', '-k', clim, '-m', 'EC-Earth3', '-v', 'debug'],
+#                     env=env, check=True)
+#     assert cmp('tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml',
+#                'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref')
+
+# # test on amip but with access from xarray dataset
+# @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
+# def test_performance_indices_amip_xdataset(clim):
+#     thefile = 'tests/table/PI4_' + clim + '_amip_EC-Earth4_r1i1p1f1_1990_1990.yml'
+#     if os.path.isfile(thefile):
+#         os.remove(thefile)
+#     xfield = xr.open_mfdataset('tests/data/amip/output/oifs/*.nc', preprocess=xr_preproc)
+#     performance_indices('amip', 1990, 1990, numproc=2, climatology=clim,
+#                         config='tests/config.yml', xdataset=xfield)
+#     assert cmp(thefile, 'tests/table/PI4_' + clim + '_amip_1990_1990.ref')
+
