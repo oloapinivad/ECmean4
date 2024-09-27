5c27a94e71d38bc43d0ad309ec8fc227f0b75a43
mnurisso <m.nurisso@isac.cnr.it>
2024-09-27 16:50:13 +0300

    tests update

--- tests/test_performance_indices.py
+++ tests/test_performance_indices.py
@@ -43,12 +43,13 @@ def test_performance_indices_amip(clim):
 
     assert are_dicts_equal(data1, data2, TOLERANCE), "YAML files are not identical."
 
+
 # test performance_indices from commnand line + debug
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
 def test_cmd_performance_indices_CMIP6(clim):
     subprocess.run(['performance_indices', 'historical', '1990', '1990', '-j', '2', '-c',
-                   'tests/config_CMIP6.yml', '-k', clim, '-m', 'EC-Earth3', '-v', 'debug'],
-                    env=env, check=True)
+                    'tests/config_CMIP6.yml', '-k', clim, '-m', 'EC-Earth3', '-v', 'debug'],
+                   env=env, check=True)
     file1 = 'tests/table/PI4_' + clim + '_historical_EC-Earth3_r1i1p1f1_1990_1990.yml'
     file2 = 'tests/table/PI4_' + clim + '_CMIP6_1990_1990.ref'
     with open(file1, 'r', encoding='utf8') as f1, open(file2, 'r', encoding='utf8') as f2:
@@ -56,6 +57,7 @@ def test_cmd_performance_indices_CMIP6(clim):
         data2 = yaml.safe_load(f2)
 
     assert are_dicts_equal(data1, data2, TOLERANCE), "YAML files are not identical."
+
 
 # test on amip but with access from xarray dataset
 @pytest.mark.parametrize("clim", ['RK08', 'EC23'])
@@ -78,6 +80,6 @@ def test_pi_plot():
     file1 = 'tests/table/PI4_EC23_amip_EC-Earth4_r1i1p1f1_1990_1990.yml'
     with open(file1, 'r', encoding='utf8') as f1:
         data1 = yaml.safe_load(f1)
-    heatmap_comparison_pi(data1, {'modelname': 'EC-Earth4', 'expname': 'amip', 'year1': '1990', 'year2': '1990'}, 'tests/pluto/PI4_EC23_amip_EC-Earth4_r1i1p1f1_1990_1990.png')
-    assert os.path.isfile('tests/pluto/PI4_EC23_amip_EC-Earth4_r1i1p1f1_1990_1990.png'), "Plot not created."
-    os.remove('tests/pluto/PI4_EC23_amip_EC-Earth4_r1i1p1f1_1990_1990.png')
+    heatmap_comparison_pi(data1, title='CMIP6 RELATIVE PI test')
+    assert os.path.isfile('PI4_heatmap.pdf'), "Plot not created."
+    os.remove('PI4_heatmap.pdf')
